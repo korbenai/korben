97cee1269b886aaa3948991decb4079d85e68731
Jonathan Cran <jcran@0x0e.org>
2025-10-19 22:22:30 -0500

    updated stories

--- src/core/registry.py
+++ src/core/registry.py
@@ -1,7 +1,6 @@
 """Registry - dictionaries of available tasks and flows."""
 
 from src.core.tasks import entropy
-from src.core.tasks import cybernews
 from src.core.tasks import download_podcasts
 from src.core.tasks import transcribe_podcasts
 from src.core.tasks import extract_wisdom
@@ -9,13 +8,15 @@ from src.core.tasks import send_email
 from src.core.tasks import read_file
 from src.core.tasks import write_file
 from src.core.tasks import markdown_to_html
+from src.core.tasks import get_mallory_stories
 from src.core.flows.podcasts import podcast_workflow
+from src.core.flows.mallory_stories import mallory_stories_workflow
 
 
 # Dictionary of all available tasks
 TASKS = {
     "entropy": entropy.run,
-    "cybernews": cybernews.run,
+    "get_mallory_stories": get_mallory_stories.run,
     "download_podcasts": download_podcasts.run,
     "transcribe_podcasts": transcribe_podcasts.run,
     "extract_wisdom": extract_wisdom.run,
@@ -28,5 +29,6 @@ TASKS = {
 # Dictionary of all available flows
 FLOWS = {
     "podcasts": podcast_workflow,
+    "mallory_stories": mallory_stories_workflow,
 }
 
